78baca13b176d0849952d02a0e0efdc55d8bf66f
Matthew Grigajtis <matthew.grigajtis@gmail.com>
2016-09-22 12:25:45 -0400

    Adds styling to home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,5 +5,24 @@ $(document).ready(function(){
         $('#gc1').toggleClass('newClass');
         $('#gc2').toggleClass('newClass');
       }
-  })
+    });
+
+    var centerIt = function (el /* (jQuery element) Element to center */) {
+      if (!el) {
+      	return;
+      }
+      var moveIt = function () {
+          var winWidth = $(window).width();
+          var winHeight = $(window).height();
+          el.css('width', (winWidth * .80) + 'px');
+          el.css('height', (winHeight * .80) + 'px');
+          el.css("position","absolute");
+          el.css("left", ((winWidth / 2) - (el.width() / 2)) + "px");
+          el.css("top", ((winHeight / 2) - (el.height() / 2)) + "px");
+      };
+      $(window).resize(moveIt);
+      moveIt();
+  };
+
+  centerIt($('.center-frame'));
 });
