47e3739512f85e6dee5dd69778d4384c9becb03d
Matthew Grigajtis <matthew.grigajtis@gmail.com>
2016-09-22 09:20:16 -0400

    Changes double quotes to single quotes

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $("#trigger").click(function(){
+    $('#trigger').click(function(){
       if ((this).is(hover))
       {
-        $("#gc1").toggleClass("newClass");
-        $("#gc2").toggleClass("newClass");
+        $('#gc1').toggleClass('newClass');
+        $('#gc2').toggleClass('newClass');
       }
   })
 });
