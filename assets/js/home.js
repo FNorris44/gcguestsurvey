498e6b4f162bca464048c34ade08a125b6398fd1
Matthew Grigajtis <matthew.grigajtis@gmail.com>
2016-11-01 14:06:57 -0400

    Fixes syntax error

--- assets/js/home.js
+++ assets/js/home.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
           var winHeight = $(window).height();
           el.css("position","absolute");
           el.css("left", (((winWidth * 0.80) / 2) - (el.width() / 2)) + "px");
-          el.css("top", ((winHeight * 0.80) / 2) - (el.height() / 2)) + "px");
+          el.css("top", (((winHeight * 0.80) / 2) - (el.height() / 2)) + "px");
       };
       $(window).resize(moveIt);
       moveIt();
