541a961a6726ef0e2f7ffbc0b62ea815039b1d34
Matthew Grigajtis <matthew.grigajtis@gmail.com>
2016-11-01 14:01:39 -0400

    Fixes centering and widths

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,11 +7,13 @@ $(document).ready(function(){
       var moveIt = function () {
           var winWidth = $(window).width();
           var winHeight = $(window).height();
-          el.css('width', (winWidth * .80) + 'px');
-          el.css('height', (winHeight * .80) + 'px');
           el.css("position","absolute");
-          el.css("left", ((winWidth / 2) - (el.width() / 2)) + "px");
-          el.css("top", ((winHeight / 2) - (el.height() / 2)) + "px");
+          el.css("left", (((winWidth * 0.80) / 2) - (el.width() / 2)) + "px");
+          el.css("top", ((*winHeight * 0.80) / 2) - (el.height() / 2)) + "px");
+
+          //el.css("left", "50%");
+          //el.css("top", "50%");
+          //el.css("-webkit-transform", "translateY(-50%) translateX(-50%)");
       };
       $(window).resize(moveIt);
       moveIt();
